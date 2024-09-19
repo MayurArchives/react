dbddfb7036f697a31266273c8cbd01e999660a68
MayurArchives <mayurkanyal10@gmail.com>
2024-09-19 22:46:57 +0530

    class componenets

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React, { Component, useState } from 'react'
 import Greet from './components/Greet';
-
+import welcome from './components/welcome';
+import Welcome from './components/welcome';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Greet></Greet>
+                <Greet> </Greet>
+                <Welcome> </Welcome >
 
-            </div>
+            </div >
         );
     }
 }
