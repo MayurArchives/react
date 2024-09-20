8edb84be72f81c9bdcb89d0b33ed1f9083feea85
MayurArchives <mayurkanyal10@gmail.com>
2024-09-20 12:28:48 +0530

    props

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import React, { Component, useState } from 'react'
 import Greet from './components/Greet';
 import welcome from './components/welcome';
 import Welcome from './components/welcome';
+import Hello from './components/hello';
+import Preet from './components/preet';
+
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <Greet> </Greet>
-                <Welcome> </Welcome >
-
+                {/* <Greet> </Greet> */}
+                {/* <Welcome> </Welcome > */}
+                {/* <Hello /> */}
+                <Preet name="Mayur" />
+                <Preet name="Monu" />
+                <Preet name="Milind" />
             </div >
         );
     }
---
+++ src/components/preet.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Preet = props => {
+    console.log(Preet)
+    return <h1>hello {props.name}</h1>
+
+}
+
+export default Preet
