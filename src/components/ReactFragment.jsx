6cd27b00ed6400d875e6d65a287c55d64e14c1d9
MayurArchives <mayurkanyal10@gmail.com>
2024-09-26 21:57:54 +0530

    Fragments shortcut

--- src/components/ReactFragment.jsx
+++ src/components/ReactFragment.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+// import React from 'react'
 
-function ReactFragment() {
+// function ReactFragment() {
+//     return (
+//         <React.Fragment>
+//             <h1>Welcome to my House</h1>
+//             <p> This is my Beautiful House</p>
+//         </React.Fragment>
+//     )
+// }
+
+// export default ReactFragment
+import React from 'react';
+
+function MyComponent() {
     return (
-        <React.Fragment>
-            <h1>Welcome to my House</h1>
-            <p> This is my Beautiful House</p>
-        </React.Fragment>
-    )
+        <>
+            <h1>Welcome to My Website</h1>
+            <p>This is a paragraph inside a fragment.</p>
+        </>
+    );
 }
 
-export default ReactFragment
+export default MyComponent;
+
